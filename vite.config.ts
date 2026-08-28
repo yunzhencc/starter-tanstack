16217c5aa3eb22701f508b7703fb4300b1cbaae1
wangxingkang <wang_xingkang@qq.com>
2026-08-28 17:35:45 +0800

    chore(dev): add code inspector plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 import { configDefaults, defineConfig } from 'vitest/config';
 
-const config = defineConfig({
+const config = defineConfig(({ command, mode }) => ({
   resolve: { tsconfigPaths: true },
   plugins: [
+    command === 'serve' && mode !== 'test' && codeInspectorPlugin({
+      bundler: 'vite',
+      editor: 'code',
+    }),
     devtools(),
     tailwindcss(),
     tanstackStart(),
@@ -16,6 +21,6 @@ const config = defineConfig({
     environment: 'jsdom',
     exclude: ['e2e/**', ...configDefaults.exclude],
   },
-});
+}));
 
 export default config;
